2db1c89fd1712d6f0a1dbc8b33f6718289303862
Quang <nmquang2012@gmail.com>
2021-06-21 11:26:03 +0700

    Update: Doesn't process icon's path

--- src/components/DayCard/index.js
+++ src/components/DayCard/index.js
@@ -2,23 +2,19 @@ import React from 'react';
 
 import './DayCard.css';
 
-export default function WeatherCard({ date, icon, tempMax, tempMin }) {
+export default function DayCard({ date, icon, tempMax, tempMin }) {
     
-    // Get icon's URL from public folder
-    // Replace ".GIF" extension to ".PNG"
-    let urlIcon = `${process.env.PUBLIC_URL}/assets/weather_icons_png/` + icon.replace('gif', 'png');
-
     const getDay = (date) => {
         const days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
         
         // Reverse date string "12/06/2021" => "2021/06/12"
-        // (hour, min, sec, ms) = 0
+        // Set (hour, min, sec, ms) = 0
         let reverseDateStr = new Date( date.split("/").reverse().join("/") );
 
         // Get day of week 
         let day = days[reverseDateStr.getDay()];
 
-        // get today, (hour, min, sec, ms) = 0
+        // Get today, set (hour, min, sec, ms) = 0
         let today = new Date();
         today.setHours(0,0,0,0);
 
@@ -32,7 +28,7 @@ export default function WeatherCard({ date, icon, tempMax, tempMin }) {
     return (
         <div className="day-card">
             <p> {getDay(date)} </p>
-            <img className="icon" src={urlIcon} alt="" width="50"/>
+            <img className="icon" src={icon} alt=""/>
             <div className="footer-temperature">
                 <p> {tempMax}&deg; </p>
                 <p> {tempMin}&deg; </p>
