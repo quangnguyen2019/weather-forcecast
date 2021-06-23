be417b25d8aa385be5e99781e832f22f775cba44
Quang <nmquang2012@gmail.com>
2021-06-23 21:45:20 +0700

    Summary Weather by Celsius degree

--- src/components/ForecastContainer/index.js
+++ src/components/ForecastContainer/index.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './ForecastContainer.css';
 import SummaryForecast from '../SummaryForecast';
 import DayCard from '../DayCard/index';
 
 export default function ForecastContainer() {
-    ///  STATE
     const [dataArr, setDataArr] = useState([]);
-
     let url = "http://api.weatherunlocked.com/api/forecast/12.496359148010487,109.12066138465396?app_id=f543a0f4&app_key=ce7a7c30aa776c359baab10a2a880db3";
     
     useEffect(() =>
@@ -15,13 +13,27 @@ export default function ForecastContainer() {
         .then(res => res.json())
         .then(data => {
             setDataArr(data.Days);
+            console.log(data.Days);
         })
     , [url]);
 
     // Get Icon's URL
-    const getIcon = (arr) => {
+    const getIconPath = (timeframe) => {
+        // Get icon's name from Timeframe
+        const iconName = timeframe.wx_icon;
 
-        // Count Frequency
+        // Get icon's URL from public folder
+        // Replace ".GIF" extension to ".PNG"
+        // Return icon's URL
+        return (
+            `${process.env.PUBLIC_URL}/assets/weather_icons_png/` + 
+            iconName.replace('gif', 'png')
+        );
+    }
+
+    const mostOccuringIcon = (arr) => {
+
+        // Count Frequency =>  Object { a: 2, b: 3, ... }
         const hashmap = arr.reduce((result, val) => {
             result[val.wx_desc] = ( result[val.wx_desc] | 0 ) + 1;
             return result;
@@ -33,23 +45,16 @@ export default function ForecastContainer() {
                   .reduce((a, b) => hashmap[a] > hashmap[b] ? a : b
             );
 
-        // Get icon's name from Array
-        const iconName = (arr.filter(x => x.wx_desc === mostFrequent))[0].wx_icon;
-
-        // Get icon's URL from public folder
-        // Replace ".GIF" extension to ".PNG"
-        // Return icon's URL
-        return (
-            `${process.env.PUBLIC_URL}/assets/weather_icons_png/` + 
-            iconName.replace('gif', 'png')
-        );
+        const timeframe = (arr.filter(x => x.wx_desc === mostFrequent))[0];
+        
+        return getIconPath(timeframe);
     };
 
     return (
         <div className="forecast-container">
             <SummaryForecast 
                 dataArr={dataArr} 
-                getIcon={getIcon}
+                getIconPath={getIconPath}
             />
 
             <div className="forecast-days">
@@ -61,7 +66,7 @@ export default function ForecastContainer() {
                         dataArr.map((val, index) =>
                             <DayCard
                                 date={val.date}
-                                icon={getIcon(val.Timeframes)}
+                                icon={mostOccuringIcon(val.Timeframes)}
                                 tempMax={Math.round(val.temp_max_c)} 
                                 tempMin={Math.round(val.temp_min_c)} 
                                 key={index} 
